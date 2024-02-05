9b2ec421c510be56d5b016a47fe026b8e710e471
serraguti <pacoserranox@gmail.com>
2024-02-05 14:19:22 +0100

    Add project files.

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //SIEMPRE DEBEMOS TENER UN METODO GET
+        public IActionResult VistaControladorPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VistaControladorPost
+            (Persona persona, string aficiones)
+        {
+            ViewData["DATOS"] = "Nombre: " + persona.Nombre
+                + ", Email: " + persona.Email
+                + ", Edad: " + persona.Edad
+                + ", Aficiones: " + aficiones;
+            return View();
+        }
+
+
+        public IActionResult ControladorVista()
+        {
+            //VAMOS A ENVIAR INFORMACION SIMPLE A NUESTRA VISTA
+            ViewData["NOMBRE"] = "Alumno";
+            ViewData["EDAD"] = 24;
+            ViewBag.DiaSemana = "Lunes";
+            ViewData["DIASEMANA"] = "Mañana martes!!!";
+            Persona persona = new Persona();
+            persona.Nombre = "Persona Model";
+            persona.Email = "model@gmail.com";
+            persona.Edad = 77;
+            return View(persona);
+        }
+
+        //VAMOS A RECIBIR DOS PARAMETROS
+        public IActionResult VistaControladorGet
+            (string app, int? version)
+        {
+            //AHORA LA VERSION ES OPCIONAL
+            if (version is null)
+            {
+
+            }
+                //DIBUJAMOS EN LA VISTA LOS PARAMETROS RECIBIDOS
+            ViewData["DATOS"] = "Application: " + app.ToUpper()
+                    + ", Version: " + version.GetValueOrDefault();
+
+            return View();
+        }
+    }
+}
---
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult SumarNumerosGet(int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["MENSAJE"] = "La suma de " + numero1
+                + " + " + numero2 + "=" + suma;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SumarNumerosPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumerosPost(int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["MENSAJE"] = "La suma de " + numero1
+                + " + " + numero2 + "=" + suma;
+            return View();
+        }
+
+        public IActionResult ConjeturaCollatz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConjeturaCollatz(int numero)
+        {
+            //DECLARAMOS UN MODEL PARA ENVIAR A LA VISTA
+            List<int> numeros = new List<int>();
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                numeros.Add(numero);
+            }
+            //DEVOLVEMOS EL MODEL A LA VISTA
+            return View(numeros);
+        }
+    }
+}
